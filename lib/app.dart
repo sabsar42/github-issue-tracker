be70ccef99948a1104d14ecab07f01a01d065a9b
Shakib Absar <sabsar42@gmail.com>
2024-10-24 01:07:44 +0600

    signOut fixed

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart';  // Use Material package instead of Cupertino for consistency
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:github_issue_tracker/app/screens/auth/sign_in.dart';
 import 'app/controller/auth_controller.dart';
 
 class GithubIssueTracker extends StatelessWidget {
   const GithubIssueTracker({super.key});
+
 
   @override
   Widget build(BuildContext context) {
